a0bcbdaebc28cfccd1dad56c6aa57c84417d35e4
Nikita Timofeev <stariy95@users.noreply.github.com>
2020-01-30 11:21:40 +0300

    Update bq.rb
    add correct checksum

--- Formula/bq.rb
+++ Formula/bq.rb
@@ -2,7 +2,7 @@ class Bq < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
   url "https://bintray.com/bootique/bq/download_file?file_path=bq-0.93.zip"
-  sha256 ""
+  sha256 "c1f929b8fe6a70d5d7b71574d5e3bc1ed14720714e4ca4283a2924b571ccfa0c"
 
   bottle :unneeded
 
