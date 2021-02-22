55bdacd304e821d421e941c17a09687e6f9f8a72
Nikita Timofeev <stariy95@gmail.com>
2021-02-22 18:53:52 +0300

    Swich to GitHub releases

--- Formula/bq.rb
+++ Formula/bq.rb
@@ -1,8 +1,8 @@
 class Bq < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
-  url "https://bintray.com/bootique/bq/download_file?file_path=bq-0.99.zip"
-  sha256 "7fbba3bdca4495228da1120ce673c601b0f1fdaad48d689a45d04558ed6f6546"
+  url "https://github.com/bootique/bootique-tool/releases/download/0.98/bq-0.98.zip"
+  sha256 "4c5fccc3f5c6cd3ce87f80ad86e2fc07443f569a727e8705a0adee6f0ba9c0e5"
 
   bottle :unneeded
 
